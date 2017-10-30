1b5f3bebc5c552612f5ff449c905627dd812f6b8
Benjamin Lasseur <benjamin.lasseur@gmail.com>
2017-10-30 14:58:56 +0100

    ajout javadoc

--- src/test/java/dev/paie/util/JeuxDeDonneesTest.java
+++ src/test/java/dev/paie/util/JeuxDeDonneesTest.java
@@ -16,25 +16,39 @@ import dev.paie.entite.Cotisation;
 
 public class JeuxDeDonneesTest {
 
+	/** context : ClassPathXmlApplicationContext */
 	private ClassPathXmlApplicationContext context;
+	/** bulletin1 : BulletinSalaire */
 	private BulletinSalaire bulletin1;
 
+	/**
+	 * Instanciation d'un bulletin de salaire
+	 */
 	@Before
 	public void onSetup() {
 		context = new ClassPathXmlApplicationContext("jdd-config.xml");
 		bulletin1 = context.getBean("bulletin1", BulletinSalaire.class);
 	}
 
+	/**
+	 * Test primeExceptionnel = 1000
+	 */
 	@Test
 	public void test_primeExceptionnelle() {
 		assertThat(bulletin1.getPrimeExceptionnelle()).isEqualTo(new BigDecimal("1000"));
 	}
 
+	/**
+	 * Test matricule = M01
+	 */
 	@Test
 	public void test_employe() {
 		assertThat(bulletin1.getRemunerationEmploye().getMatricule()).isEqualTo("M01");
 	}
 
+	/**
+	 * Test sur l'entreprise
+	 */
 	@Test
 	public void test_entreprise() {
 		assertThat(bulletin1.getRemunerationEmploye().getEntreprise().getSiret()).isEqualTo("80966785000022");
@@ -42,6 +56,9 @@ public class JeuxDeDonneesTest {
 		assertThat(bulletin1.getRemunerationEmploye().getEntreprise().getCodeNaf()).isEqualTo("6202A");
 	}
 
+	/**
+	 * Test sur les cotisations non-imposables
+	 */
 	@Test
 	public void test_cotisationsNonImposables() {
 		List<Cotisation> cotisationsNonImposables = bulletin1.getRemunerationEmploye().getProfilRemuneration()
@@ -51,6 +68,9 @@ public class JeuxDeDonneesTest {
 				.forEach(code -> assertThat(cotisationsNonImposables.stream().filter(c -> c.getCode().equals(code)).findAny().isPresent()).isTrue());
 	}
 
+	/**
+	 * Test sur les cotisations imposables
+	 */
 	@Test
 	public void test_cotisationImposables() {
 		List<Cotisation> cotisationsImposables = bulletin1.getRemunerationEmploye().getProfilRemuneration()
@@ -58,12 +78,18 @@ public class JeuxDeDonneesTest {
 		Stream.of("SP01", "SP02").forEach(code -> assertThat(cotisationsImposables.stream().filter(c -> c.getCode().equals(code)).findAny().isPresent()).isTrue());
 	}
 
+	/**
+	 * Test sur grade
+	 */
 	@Test
 	public void test_grade() {
 		assertThat(bulletin1.getRemunerationEmploye().getGrade().getNbHeuresBase()).isEqualTo(new BigDecimal("151.67"));
 		assertThat(bulletin1.getRemunerationEmploye().getGrade().getTauxBase()).isEqualTo(new BigDecimal("11.0984"));
 	}
 
+	/**
+	 * Fermeture du context
+	 */
 	@After
 	public void onExit() {
 		context.close();
--- src/test/java/dev/paie/util/PaieUtilsTest.java
+++ src/test/java/dev/paie/util/PaieUtilsTest.java
@@ -12,32 +12,49 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class PaieUtilsTest {
+	/** paieUtils : PaieUtils */
 	private PaieUtils paieUtils;
+	/** context : ClassPathXmlApplicationContext */
 	private ClassPathXmlApplicationContext context;
+	/**
+	 * Création du contexte
+	 */
 	@Before
 	public void onSetup() {
 		context = new ClassPathXmlApplicationContext("app-config.xml");
 		paieUtils = context.getBean(PaieUtils.class);
 	}
 
+	/**
+	 * Test
+	 */
 	@Test
 	public void test_formaterBigDecimal_entier_positif() {
 		String resultat = paieUtils.formaterBigDecimal(new BigDecimal("2"));
 		assertThat(resultat, equalTo("2.00"));
 	}
 
+	/**
+	 * Test
+	 */
 	@Test
 	public void test_formaterBigDecimal_trois_chiffres_apres_la_virgule() {
 		String resultat = paieUtils.formaterBigDecimal(new BigDecimal("2.199"));
 		assertThat(resultat, equalTo("2.20"));
 	}
 	
+	/**
+	 * Test
+	 */
 	@Test
 	public void test_formaterBigDecimal_quatre_chiffres_apres_la_virgule() {
 		String resultat = paieUtils.formaterBigDecimal(new BigDecimal("2.1999"));
 		assertThat(resultat, equalTo("2.20"));
 	}
 
+	/**
+	 * Fermeture du context
+	 */
 	@After
 	public void onExit() {
 		context.close();
